bfe1424e5d905738f7c01d6f654e5b56dee3808f
wizjaunty2 <wizjaunty2@gmail.com>
2022-09-09 07:01:45 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  *  * main - Entry point
- *   *
+ *   *   *
  *    * Return: Always 0 (Success)
- *     * print function "puts" not "printsf"
- **/
+ *     **/
+
 int main(void)
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-	        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
