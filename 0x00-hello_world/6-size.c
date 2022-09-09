29cd63e6362cd5a38b2b07f71cc963092813b8e6
wizjaunty2 <wizjaunty2@gmail.com>
2022-09-09 14:28:16 -0700

    size type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/*
+ * main - entry point
+ * return: always 0 (sucess)
+ */
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n")
+	printf("Size of an int: 4 byte(s)\n")
+	printf("Size of a long int: 4 byte(s)\n")
+	printf("Size of a long long int: 8 byte(s) \n")
+	printf("Size of a float: 4 byte(s)\n")
+}
