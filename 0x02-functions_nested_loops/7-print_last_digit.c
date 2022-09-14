0a1417153a05dfd0389252d8ea7f3ae2ac67b18f
ekomobong maurice okon <109705317+wizjaunty2@users.noreply.github.com>
2022-09-14 01:03:10 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,12 @@
  * Return: last digit of number
  */
 
-int print_last_digit(int n)
+int print_last_digit(int r)
 {
-	if (n < 0)
+	if (r < 0)
 		n *= -1;
 
-       	_putchar('0' + (n % 10));
+       	_putchar('0' + (r % 10));
 
-	return (n % 10);
+	return (r % 10);
 }
