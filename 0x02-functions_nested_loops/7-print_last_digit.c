f2716998dc5c9f2ff484a79676fe29fdfbad695c
ekomobong maurice okon <109705317+wizjaunty2@users.noreply.github.com>
2022-09-14 01:48:24 +0100

    function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,6 +6,7 @@
  *
  * Return: Value of the last digit.
  */
+
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
@@ -14,6 +15,5 @@ int print_last_digit(int n)
 	last_digit *= -1;
 	
 	_putchar(last_digit + '0');
-
 	return (last_digit);
 }
