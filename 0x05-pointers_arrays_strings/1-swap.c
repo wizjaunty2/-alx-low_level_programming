f2d109fb69d58b550dbd5a39c5d30aa9e5ec7f9d
ekomobong maurice okon <109705317+wizjaunty2@users.noreply.github.com>
2022-09-20 01:15:04 +0100

    Swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 /**
- * swap_int - swaps the values of two integers.
- */
+* main - check the code
+* Return: Always 0.
+* swap_int - swaps the values of two integers.
+*/
+
 void swap_int(int *a, int *b)
 {
+int swp = *a;
 *a = *b;
-*b = *a;
+*b = *swp;
 }
