54b092ad7ff5ca8acf4bc14fed84436328ed1630
ekomobong maurice okon <109705317+wizjaunty2@users.noreply.github.com>
2022-09-20 02:39:13 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+		put(*str++);
+
+	put('\n');
+}
