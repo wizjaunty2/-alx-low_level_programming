0d1859b07e8c7024078501df2edf1358b883511e
ekomobong maurice okon <109705317+wizjaunty2@users.noreply.github.com>
2022-09-20 02:45:22 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
@@ -7,8 +7,7 @@
 
 void _puts(char *str)
 {
-	while (*str != '\0')
-		puts(*str++);
-
-	puts('\n');
+while (*str != '\0')
+_putchar(*str++);
+_putchar('\n');
 }
